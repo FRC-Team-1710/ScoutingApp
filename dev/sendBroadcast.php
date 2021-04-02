0786194dbfcb0240c9c862cf519ec409635759c4
Charlie <charliusb@gmail.com>
2021-04-02 17:30:05 -0500

    dev broadcasts are now synced to the new system

--- dev/sendBroadcast.php
+++ dev/sendBroadcast.php
@@ -24,14 +24,25 @@
     }
 
     $msg = $_POST["broadcast"];
-    $team = 34522;
     $author = "Developer Team";
+    $date = date("m/d/y");
     $time = date("h:i");
 
-    //Generate Permission Value
-    $perms = 111111;
+    $badCode = true;
+    while($badCode) {
+        $badCode = false;
+        $permittedChars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        $code = substr(str_shuffle($permittedChars), 0, 10);
+        $sql = "SELECT * FROM notifications";
+        $result = $conn->query($sql);
+        while($row = $result->fetch_assoc()) {
+            if($row["Code"] == $code) {
+                $badCode = true;
+            }
+        }
+    }
 
-    $sql = "INSERT INTO notifications (Team, Permission, Author, Message, Time) VALUES ('$team', '$perms', '$author', ?, '$time')";
+    $sql = "INSERT INTO notifications (Team, Permission, Author, Message, Time, Date, Code) VALUES ('$team', '$perms', '$author', ?, '$time', '$date', '$code')";
     $stmt = mysqli_stmt_init($conn);
     if(!mysqli_stmt_prepare($stmt, $sql)) {
         echo "There was an error";
@@ -39,9 +50,17 @@
         mysqli_stmt_bind_param($stmt, "s", $msg);
         mysqli_stmt_execute($stmt);
     }
-    $notification = 1;
 
-    $sql = "UPDATE users SET unreadNotification = '$notification'";
+    $sql = "UPDATE users SET unreadNotification = 1";
+    $result = $conn->query($sql);
+    $sql = "SELECT notificationReference FROM users WHERE Username = '$uname'";
+    $result = $conn->query($sql);
+    while($row = $result->fetch_assoc()) {
+        $refs = $row["notificationReference"];
+    }
+    $code = "," . $code;
+
+    $sql = "UPDATE users SET notificationReference = CONCAT(notificationReference, '$code')";
     $result = $conn->query($sql);
 
     $_SESSION["alert"] = true;
