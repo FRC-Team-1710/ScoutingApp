84d04b9aaa389eb51d6bbe2922016212059cc01e
Charlie <charliusb@gmail.com>
2021-04-09 15:43:41 -0500

    icon update

--- hub.php
+++ hub.php
@@ -39,6 +39,7 @@
     <title>Team 1710 Scouting App</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link rel="icon" href="logo.jpg">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
